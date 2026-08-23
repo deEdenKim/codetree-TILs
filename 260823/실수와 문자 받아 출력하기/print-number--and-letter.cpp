857e8a36094bdd6d16734a22eb7f3088ef18c9a6
deEdenKim <101259627+deEdenKim@users.noreply.github.com>
2026-08-23 11:23:01 +0100

    [Passed] 실수와 문자 받아 출력하기 (2ms, 0MB)

---
+++ 260823/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c;
+    cin>>c;
+    double a,b;
+    cin>>a>>b;
+
+    
+    cout<<c<<"\n";
+    cout<<fixed;
+    cout.precision(2);
+    cout<<a<<"\n"<<b;
+    return 0;
+}
