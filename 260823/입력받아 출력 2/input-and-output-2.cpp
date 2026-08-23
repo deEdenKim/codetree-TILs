a2e5495747d9dd80de73e1b9d26871ba8f8b267a
deEdenKim <101259627+deEdenKim@users.noreply.github.com>
2026-08-23 11:41:17 +0100

    [Passed] 입력받아 출력 2 (2ms, 0MB)

---
+++ 260823/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int first;
+    int behind;
+    cin>> first;
+    cin.get();
+    cin>>behind;
+
+    cout<<first<<behind;
+    return 0;
+}
