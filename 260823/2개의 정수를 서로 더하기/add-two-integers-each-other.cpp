661acb9a55a7d0014128c8041671e9ed517c7f33
deEdenKim <101259627+deEdenKim@users.noreply.github.com>
2026-08-23 11:53:56 +0100

    [Passed] 2개의 정수를 서로 더하기 (2ms, 0MB)

---
+++ 260823/2개의 정수를 서로 더하기/add-two-integers-each-other.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>> a>>b;
+    
+    a+=b;
+    cout<<a<<" ";
+    b+=a;
+    cout<<b;
+    return 0;
+}
