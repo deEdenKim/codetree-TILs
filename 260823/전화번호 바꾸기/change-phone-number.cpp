4c3d4707ee8bb69aa6f64e6a9ad284d2b0f4cff8
deEdenKim <101259627+deEdenKim@users.noreply.github.com>
2026-08-23 11:44:15 +0100

    [Passed] 전화번호 바꾸기 (2ms, 0MB)

---
+++ 260823/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int xxxx,yyyy;
+    cin.get();
+    cin.get();
+    cin.get();
+    cin.get();
+    cin>>xxxx;
+    cin.get();
+    cin>>yyyy;
+
+    cout<<"010-"<<yyyy<<"-"<<xxxx;
+    return 0;
+}
